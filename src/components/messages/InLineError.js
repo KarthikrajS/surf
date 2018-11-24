ddc37fdc73acb7372b7f9586289735cb07f058ff
KarthikrajS <karthikraj1995@gmail.com>
2018-11-24 20:20:21 +0530

    basic login

---
+++ src/components/messages/InLineError.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InLineError = ({ text }) =>(
+    <span style={ {color : "#ae5856"}}> { text } </span>
+);
+
+InLineError.propTypes = {
+    text : PropTypes.string.isRequired
+};
+
+export default InLineError;
--- src/components/pages/Login Page.js
+++ src/components/pages/Login Page.js
@@ -1,9 +1,20 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-    <div>
-        <h1>Login Page</h1>
-    </div>
-);
+class LoginPage extends React.Component {
+
+    submit = (data) =>
+        console.log(data);
+        // this.props.login(data).then(() =>
+        // this.props.history.push("/dashboard"));
+
+    render() {
+        return (<div>
+            <h1>Login Page</h1>
+            <LoginForm submit={this.submit}/>
+        </div>)
+    }
+}
 
 export default LoginPage;
